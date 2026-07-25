325bf36096b69e07878363702bf84c3c51ce1ecf
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-07-25 09:52:31 +0530

    update second largest number

--- arrays/02-second-largest.js
+++ arrays/02-second-largest.js
@@ -1,28 +1,41 @@
 // solution the first largest number
+/**
+ * Problem: Find the Largest Element in an Array
+ * Topic: Arrays
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+*/
+// Solution
+
+console.log(firstLargest([2, 5, 6, 3, 5, 99, 555]));
 function firstLargest(arr) {
     let largest = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (largest < arr[i]) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
             largest = arr[i];
         }
     }
     return largest;
 }
 
-console.log(firstLargest([2, 5, 6, 3, 5, 99, 555]))
-
+/**
+ * Problem: Find the Second Largest Element in an Array
+ * Topic: Arrays
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+*/
+// Solution
+console.log(secondLargest([5, 6, 8, 56, 4, 5]));
 function secondLargest(arr) {
-    let largest = 0;
-    let secondLarge = 0;
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > largest) {
-            secondLarge = largest;
+            secondLargest = largest;
             largest = arr[i];
-        } else if (arr[i] > secondLarge && arr[i] !== largest) {
-            secondLarge = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] !== largest) {
+            secondLargest = arr[i];
         }
     }
-    return secondLarge;
-
+    return secondLargest;
 }
-console.log(secondLargest([5, 6, 8, 56, 4, 5]))
